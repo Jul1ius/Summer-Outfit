e1e20ace72eb1a802b3e4f54fa3c107d3f9f40f9
its julii <nanji828@gmail.com>
2022-11-17 17:49:02 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Reflection.Metadata;
+
+namespace Summer_Outfit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int degrees = int.Parse(Console.ReadLine());
+            string time = Console.ReadLine();
+            string outfit = "";
+            string shoes = "";
+
+            if (10 <= degrees && degrees <= 18)
+            {
+
+                switch (time)
+                {
+                    case "Morning":
+                        outfit = "Sweatshirt";
+                        shoes = "Sneakers";
+                        break;
+                    case "Afternoon":
+                        outfit = "Shirt";
+                        shoes = "Moccasins";
+                        break;
+                    default:
+                        outfit = "Shirt";
+                        shoes = "Moccasins";
+                        break;
+
+                }
+            }
+            else if (18 <= degrees && degrees <= 24)
+            {
+                switch (time)
+                {
+                    case "Morning":
+                        outfit = "Shirt";
+                        shoes = "Moccasins";
+                        break;
+                    case "Afternoon":
+                        outfit = "T-Shirt";
+                        shoes = "Sandals";
+                        break;
+                    default:
+                        outfit = "Shirt";
+                        shoes = "Moccasins";
+                        break;
+
+                }
+            }
+            else
+            {
+                switch (time)
+                {
+                    case "Morning":
+                        outfit = "T-Shirt";
+                        shoes = "Sandals";
+                        break;
+                    case "Afternoon":
+                        outfit = "Swim Suit";
+                        shoes = "Barefoot";
+                        break;
+                    default:
+                        outfit = "Shirt";
+                        shoes = "Moccasins";
+                        break;
+
+                }
+            }
+
+
+            Console.WriteLine($"It's {degrees} degrees, get your {outfit} and {shoes}.");
+
+
+
+
+
+
+
+        }
+            }
+        }
+    
+
